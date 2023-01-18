860e8f65856ab7f064ed86921f7b011e163773d9
Vegadrone <spenaantonino@gmail.com>
2023-01-18 19:22:57 +0100

    feat: :sparkles: Add scarfy character to window instead of the blue rectangle

--- dasher.cpp
+++ dasher.cpp
@@ -8,26 +8,32 @@ int main(){
     const int windowWidth {800};
     const int windowHeight {500};
 
-    //Rectangle dimensions
-    const int rectWidth {50};
-    const int rectHeight{80};
-
-    //Rectangle position
-    int posY {windowHeight - rectHeight};
-
     //Rectangle velocity
     int velocity {};
 
     //Accelleration due to gravity ((pixels * frame) * frame)
     const int gravity {1};
 
+    //Initialize the window
+    //Mettilo SEMPRE prima del caricamento delle texture o da errori!
+    InitWindow(windowWidth, windowHeight, "Dapper Dasher");
+
+    //Texture
+    Texture2D scarfy = LoadTexture("textures/scarfy.png");
+    Rectangle scarfyRec;
+    scarfyRec.width = scarfy.width/6;
+    scarfyRec.height = scarfy.height;
+    scarfyRec.x = 0;
+    scarfyRec.y = 0;
+    Vector2 scarfyPos;
+    scarfyPos.x = windowWidth/2 - scarfyRec.width/2;
+    scarfyPos.y = windowHeight - scarfyRec.height;
+
     //is in Air?
     bool isInAir{};
     //JumpVelocity
     const int JumpVel{-20};
 
-    //Initialize the window
-    InitWindow(windowWidth, windowHeight, "Dapper Dasher");
     
     //Setting FPS
     SetTargetFPS(60);
@@ -38,22 +44,22 @@ int main(){
         BeginDrawing();
         ClearBackground(WHITE);
 
-        //Draw rectangle
-        DrawRectangle((windowWidth/2), posY, rectWidth, rectHeight, BLUE);
 
         //Ground Check
         /*
             Se metti == quando salti posY non è più uguale a wH - rH di conseguenza la velocità non
             è più zero come nell'if ma va nell'else e il rettangolo sprofonda
         */
-        if (posY >= (windowHeight - rectHeight))
+        if (scarfyPos.y >= (windowHeight - scarfyRec.height))
         {
             velocity = 0;
+            //In questo caso il rettangolo/character è a terra quindi setto a false
             isInAir = false;
         }
         else
         {
             velocity += gravity;
+            // In questo caso il rettangolo/character è in aria quindi setto a true
             isInAir = true;
         }
         
@@ -64,11 +70,13 @@ int main(){
         }        
 
         //Update Position
-        posY += velocity;
+        scarfyPos.y += velocity;
+        //Draw rectangle
+        DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
         
         //Stop Drawing
         EndDrawing();
     }
-
+    UnloadTexture(scarfy);
     CloseWindow();
 }
