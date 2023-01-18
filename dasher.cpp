589e9d675593babc9578965d4013593a5d74924f
Vegadrone <spenaantonino@gmail.com>
2023-01-18 16:43:08 +0100

    feat: :sparkles: Add recangle e jumping mechanic, no gravity yet

--- dasher.cpp
+++ dasher.cpp
@@ -7,6 +7,17 @@ int main(){
     //Window dimensions
     const int windowWidth {800};
     const int windowHeight {500};
+
+    //Rectangle dimensions
+    const int rectWidth {50};
+    const int rectHeight{80};
+
+    //Rectangle position
+    int posY {windowHeight - rectHeight};
+
+    //Rectangle velocity
+    int velocity {};
+
     //Initialize the window
     InitWindow(windowWidth, windowHeight, "Dapper Dasher");
     
@@ -19,9 +30,19 @@ int main(){
         BeginDrawing();
         ClearBackground(WHITE);
 
+        //Draw rectangle
+        DrawRectangle((windowWidth/2), posY, rectWidth, rectHeight, BLUE);
+
+        //Rectangle Jump
+        if (IsKeyPressed(KEY_SPACE))
+        {        
+            velocity -= 10;
+        }
+        posY += velocity;
+        
         //Stop Drawing
         EndDrawing();
     }
-    
+
     CloseWindow();
 }
